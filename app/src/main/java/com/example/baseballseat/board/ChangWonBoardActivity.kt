c046078ba0b4f188f6dd554328e23ad28bfa81df
qkrdydrjs7@naver.com <qkrdydrjs7@naver.com>
2021-11-11 23:46:46 +0900

    2021/11/11 firebase realtime database 데이터를 최신 게시물이 먼저 오도록 설정했습니다. 게시글 RecyclerView의 ViewHolder를 개선했습니다.

--- app/src/main/java/com/example/baseballseat/board/ChangWonBoardActivity.kt
+++ app/src/main/java/com/example/baseballseat/board/ChangWonBoardActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.baseballseat.*
@@ -14,7 +13,6 @@ import com.example.baseballseat.databinding.ActivityChangWonBoardBinding
 import com.facebook.login.LoginManager
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.*
-import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_chang_won_board.*
@@ -26,8 +24,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 class ChangWonBoardActivity : AppCompatActivity() {
     val TAG = "ChangWonBoardActivity"
     var userData = UserData
-    private lateinit var username : String
-    private lateinit var binding : ActivityChangWonBoardBinding//뷰 바인딩
+    private lateinit var username: String
+    private lateinit var binding: ActivityChangWonBoardBinding//뷰 바인딩
+    private var boardDataList = ArrayList<BoardData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,7 +43,7 @@ class ChangWonBoardActivity : AppCompatActivity() {
 
         binding.createBtn.setOnClickListener {
             val createpostIntent = Intent(this, CreateChangwonPostActivity::class.java)
-            createpostIntent.putExtra("local","changwon")
+            createpostIntent.putExtra("local", "changwon")
 
             startActivity(createpostIntent)
         }
@@ -52,24 +51,21 @@ class ChangWonBoardActivity : AppCompatActivity() {
         //로그아웃 버튼 클릭시
         binding.logoutBtn.setOnClickListener {
             Firebase.auth.signOut()
-            LoginManager.getInstance().logOut()//이 라인을 적지 않으면 Firebase에만 로그아웃이 되고, Facebook은 로그아웃이 안된다.
+            LoginManager.getInstance()
+                .logOut()//이 라인을 적지 않으면 Firebase에만 로그아웃이 되고, Facebook은 로그아웃이 안된다.
             Log.d(TAG, "로그아웃 시도")
             userData.username = ""
             startActivity(Intent(this, LoginActivity::class.java))
         }
 
-        var boardDataList : ArrayList<BoardData> = ArrayList()
+
         var database = FirebaseDatabase.getInstance()
-
         val adapter = BoardDataAdapter(boardDataList)
-        binding.NCBoardRv.adapter = adapter
-
         //데이터베이스에 있는 데이터를 읽어오는 코드
         val childEventListener = object : ChildEventListener {
             override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
                 Log.d(TAG, "onChildAdded:" + dataSnapshot.key!!)
 
-                // A new comment has been added, add it to the displayed list
                 val boarddata = dataSnapshot.getValue<BoardData>()
                 var bdataKey = dataSnapshot.key
                 var area = boarddata?.area
@@ -80,6 +76,7 @@ class ChangWonBoardActivity : AppCompatActivity() {
 
                 Log.d(TAG, "key : ${bdataKey} / area : ${area} / seat : ${seat} / contents : ${contents}")
                 boardDataList.add(BoardData(area, bitmapString, contents, seat, username, bdataKey))
+                boardDataList.sortByDescending { it.date }//최신 게시물일수록 먼저 나와야 하기 때문에 날짜 순대로 내림차순 정렬
                 adapter.notifyDataSetChanged()
             }
 
@@ -97,16 +94,20 @@ class ChangWonBoardActivity : AppCompatActivity() {
 
             override fun onCancelled(databaseError: DatabaseError) {
                 Log.w(TAG, "postComments:onCancelled", databaseError.toException())
-                Toast.makeText(this@ChangWonBoardActivity, "Failed to load comments.",
-                    Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this@ChangWonBoardActivity, "Failed to load comments.",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
-        val ref = database.getReference("changwon")
+        val ref = database.getReference("changwon").orderByKey()
         ref.addChildEventListener(childEventListener)
 
-        //RecyclerView를 설정하는 코드
-        Log.d(TAG, "listsize : ${boardDataList.size}")
+
+        Log.d(TAG, "list size : ${boardDataList.size}")
+        binding.NCBoardRv.adapter = adapter
         binding.NCBoardRv.layoutManager = LinearLayoutManager(this)
+
     }
 
     override fun onBackPressed() {
